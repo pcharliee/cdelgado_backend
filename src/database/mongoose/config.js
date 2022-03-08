c7f0864c914a99a125fbe19b36bbb7680394b444
Carlos Delgado <carlosm.delgador@gmail.com>
2022-03-08 09:44:02 -0300

    Update config.js

--- src/database/mongoose/config.js
+++ src/database/mongoose/config.js
@@ -2,9 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const mongoConfig = {
-//   user:"carlos",
-//   pwd:"peluli2210",
-//   name:"ecommerce"
   user: process.env.MONGO_USER,
   pwd:  process.env.MONGO_PWD,
   name: process.env.MONGO_NAME
