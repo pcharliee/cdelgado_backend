432bc1d53dc257e832cf8297404cf83e0ed9c2f1
Carlos Delgado <carlosm.delgador@gmail.com>
2022-03-16 20:12:46 -0300

    Added successRedirect for FB

--- src/utils/middlewares.js
+++ src/utils/middlewares.js
@@ -2,7 +2,10 @@ import passport from 'passport';
 
 export const passportCall = function (strategy) {
   return async function (req, res, next) {
-    return passport.authenticate(strategy, { failureRedirect: '/login' }, function (err, user, info) {
+    return passport.authenticate(strategy, {
+      failureRedirect: '/login',
+      successRedirect: '/ecommerce'
+    }, function (err, user, info) {
       if (err)   return next(err);
       if (!user) return res.send({ error: info.messages || info.toString() });
       
